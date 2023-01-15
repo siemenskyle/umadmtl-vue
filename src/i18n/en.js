c98f35898c4c21217e0d5d01116e94fbd8706b0a
Kyle Siemens <siemenskyle@gmail.com>
2023-01-15 17:58:06 -0500

    fix typos in en

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -18,28 +18,28 @@ const messages = {
         cards: {
             card1: {
                 title: "Weekly Gatherings",
-                description: "We host weekly gatherings for players of all skill levels, with a focus on community growth and friendly competiton. All are welcome to join!",
+                description: "We host weekly gatherings for players of all skill levels, with a focus on community growth and friendly competition. All are welcome to join!",
                 chip1: "Welcoming",
                 chip2: "Friendly",
                 chip3: "Local",
             },
             card2: {
                 title: "Monthly Tournaments",
-                description: "Monthly tournaments gather players to test their skills in a competative setting, drawing in players from across the region who want to show off what they can do!",
+                description: "Monthly tournaments gather players to test their skills in a competitive setting, drawing in players from across the region who want to show off what they can do!",
                 chip1: "Competition",
                 chip2: "Tournament",
                 chip3: "Regional",
             },
             card3: {
                 title: "Premiere Events",
-                description: "Our premiere events, UMAD (Ultimate Montreal Air Dashers) and WEMAD (Winter Edition Montreal Air Dashers), gather players from across the world to Montreal for multiple days of exiting competiton and socialization alike!",
+                description: "Our premiere events, UMAD (Ultimate Montreal Air Dashers) and WEMAD (Winter Edition Montreal Air Dashers), gather players from across the world to Montreal for multiple days of exiting competition and socialization alike!",
                 chip1: "Tournament",
                 chip2: "International",
             }
         },
         section2: {
             title: "Welcoming all players from all walks of life",
-            description: "Young or old, experienced or new, casual or competative, english, french or whatever, we strive to accept and welcome everyone to experience and enjoy fighting games together! Join our friendly and accepting Discord and follow us on social media to connect with us yourself!",
+            description: "Young or old, experienced or new, casual or competitive, english, french or whatever, we strive to accept and welcome everyone to experience and enjoy fighting games together! Join our friendly and accepting Discord and follow us on social media to connect with us yourself!",
         },
         section3: {
             title: "Fighting Game Events",
