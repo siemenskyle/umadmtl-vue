aba7d204be168e2fcbdf9c894e0a27d254c1a519
Kyle Siemens <siemenskyle@gmail.com>
2023-02-26 00:03:09 -0500

    add french style ! marks

--- src/i18n/fr.js
+++ src/i18n/fr.js
@@ -2,49 +2,49 @@ export {messages};
 
 const messages = {
     socials: {
-        facebook: "Rejoignez le groupe Facebook!",
-        twitter: "Suivez-nous sur Twitter!",
-        discord: "Rejoignez-nous sur Discord!",
-        youtube: "Abonnez-vous à nous sur Youtube!",
-        twitch: "Suivez-nous sur Twitch!",
+        facebook: "Rejoignez le groupe Facebook !",
+        twitter: "Suivez-nous sur Twitter !",
+        discord: "Rejoignez-nous sur Discord !",
+        youtube: "Abonnez-vous à nous sur Youtube !",
+        twitch: "Suivez-nous sur Twitch !",
         contact: "Contactez-Nous",
     },
     landing: {
         top: {
             mainTitle: "Montréal Airdashers",
             subTitle: "Une communauté locale de jeux de combat.",
-            description: "Organisation de tournois et développement d'une communauté de jeux de combat à Montréal pour amateurs et pros depuis 2009!",
+            description: "Organisation de tournois et développement d'une communauté de jeux de combat à Montréal pour amateurs et pros depuis 2009 !",
         },
         cards: {
             card1: {
                 title: "Rassemblements Hebdomadaires",
-                description: "Nous organisons des rassemblements à chaque semaine pour des joueurs de tous niveaux, avec un focus sur l'amélioration et la competition amicale. Tous sont bienvenues!",
+                description: "Nous organisons des rassemblements à chaque semaine pour des joueurs de tous niveaux, avec un focus sur l'amélioration et la competition amicale. Tous sont bienvenues !",
                 chip1: "Accueillant",
                 chip2: "Amical",
                 chip3: "Local",
             },
             card2: {
                 title: "Tournois mensuels",
-                description: "Les tournois mensuels rassemblent les joueurs désirant se prouver dans un environement compétitif, attirant des joueurs de partout dans la région pour montrer ce dont ils sont capables!",
+                description: "Les tournois mensuels rassemblent les joueurs désirant se prouver dans un environement compétitif, attirant des joueurs de partout dans la région pour montrer ce dont ils sont capables !",
                 chip1: "Competition",
                 chip2: "Tournois",
                 chip3: "Régional",
             },
             card3: {
                 title: "Événements Importants",
-                description: "Nos événements Importants, UMAD (Ultimate Montreal Air Dashers) et WEMAD (Winter Edition Montreal Air Dashers), rassemblent des joueurs de partout dans le monde à Montréal pour un événement qui s'étend sur plusieurs jours d'excitante competition et socialisation!",
+                description: "Nos événements Importants, UMAD (Ultimate Montreal Air Dashers) et WEMAD (Winter Edition Montreal Air Dashers), rassemblent des joueurs de partout dans le monde à Montréal pour un événement qui s'étend sur plusieurs jours d'excitante competition et socialisation !",
                 chip1: "Tournois",
                 chip2: "International",
             }
         },
         section2: {
             title: "Accueillant des joueurs de tous les horizons de la vie",
-            description: "Vieux ou jeunes, veterans ou débutants, amateur ou compétitif, francophone, anglophones ou peu importe, tous sont la bienvenue de venir s'amuser avec les jeux de combat ensemble! Joignez-vous à notre Discord social et suivez-nous sur les medias sociaux pour vous connecter à nous!",
+            description: "Vieux ou jeunes, veterans ou débutants, amateur ou compétitif, francophone, anglophones ou peu importe, tous sont la bienvenue de venir s'amuser avec les jeux de combat ensemble ! Joignez-vous à notre Discord social et suivez-nous sur les medias sociaux pour vous connecter à nous !",
         },
         section3: {
             title: "Rassemblements de jeux de combat",
             subTitle: "Pour Tous",
-            blurb: "En plus d'attirer des joueurs de partout dans le monde pour nos événements Importants, nous rassemblons les joueurs locaux pour des soirees de plaisirs. Montreal Airdashers a un historique prouvé d'excellents événements!",
+            blurb: "En plus d'attirer des joueurs de partout dans le monde pour nos événements Importants, nous rassemblons les joueurs locaux pour des soirees de plaisirs. Montreal Airdashers a un historique prouvé d'excellents événements !",
             partners: "Partenaires d'événements passés",
         },
     },
